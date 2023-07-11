a0a228ce7535649fea4485c05b8ac5cabead8c5a
Ankush523 <ankush.ad050203@gmail.com>
2023-07-12 00:42:04 +0530

    fe setup

---
+++ components/Homepage.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const Homepage = () => {
+  return (
+    <div>
+        <label className='text-4xl font-'>Passive Wallet</label>
+    </div>
+  )
+}
+
+export default Homepage
